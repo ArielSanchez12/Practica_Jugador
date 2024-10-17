023b26145074537c7770c6d0561551feddd7bbe3
ArielSanchez12 <riveraariel433@gmail.com>
2024-10-17 09:30:16 -0500

    Practica de la clase 4

---
+++ src/Jugador.java
@@ -0,0 +1,39 @@
+public class Jugador {
+    String avatar;
+    int edad;
+    String pais;
+    int num_jugador;
+
+    public Jugador() {
+        avatar = "Ariel"; //Para constructores vacios no hace falta usar el this.variable = variable
+        edad = 21;
+        pais = "Ecuador";
+        num_jugador = 1;
+    }
+
+    public Jugador(String avatar, int edad, String pais, int num_jugador) {
+        this.avatar = avatar; //Aqui si se usa el this.variable = variable ya que la variable se llama igual en ambos lados y el programa necesita diferenciarlos
+        this.edad = edad;
+        this.pais = pais;
+        this.num_jugador = num_jugador;
+    }
+
+
+    public void patear(){
+        System.out.println("El jugador " + num_jugador + " de nombre: " + avatar +
+                            ", edad: " + edad + ", pais: " + pais +
+                            ", ha realizado la accion patear");
+    }
+
+    public void correr(){
+        System.out.println("El jugador " + num_jugador + " de nombre: " + avatar +
+                ", edad: " + edad + ", pais: " + pais +
+                ", ha realizado la accion correr");
+    }
+
+    public void saltar(){
+        System.out.println("El jugador " + num_jugador + " de nombre: " + avatar +
+                ", edad: " + edad + ", pais: " + pais +
+                ", ha realizado la accion saltar");
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        Jugador jugador1 = new Jugador();
+        Jugador jugador2 = new Jugador("Richard", 22, "Peru", 2);
+
+        System.out.println("Acciones realizadas por el jugador 1: ");
+        jugador1.patear();
+        jugador1.correr();
+
+        System.out.println("\nAcciones realizadas por el jugador 2: ");
+        jugador2.patear();
+        jugador2.correr();
+        jugador2.saltar();
+    }
+}
